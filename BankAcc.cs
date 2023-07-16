5c78781ef99fdde1725020db310d94e0e0643e03
gusti19 <127907756+gusti19@users.noreply.github.com>
2023-07-16 15:31:31 +0700

    Add files via upload

---
+++ BankAcc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class AkunBank
+{
+    private string noRek; // Variabel untuk menyimpan nomor rekening.
+    private string pin; // Variabel untuk menyimpan PIN (Personal Identification Number) akun.
+    private decimal saldo; // Variabel untuk menyimpan saldo akun.
+
+    public string NoRek
+    {
+        get { return noRek; } // Getter untuk mendapatkan nomor rekening.
+        set { noRek = value; } // Setter untuk mengatur nomor rekening.
+    }
+
+    public string Pin
+    {
+        get { return pin; } // Getter untuk mendapatkan PIN.
+        set { pin = value; } // Setter untuk mengatur PIN.
+    }
+
+    public decimal Saldo
+    {
+        get { return saldo; } // Getter untuk mendapatkan saldo akun.
+        set { saldo = value; } // Setter untuk mengatur saldo akun.
+    }
+
+    public void Setor(decimal jumlah)
+    {
+        saldo += jumlah; // Metode untuk melakukan setor uang ke akun (menambahkan jumlah ke saldo).
+    }
+
+    public void Tarik(decimal jumlah)
+    {
+        if (jumlah <= saldo)
+        {
+            saldo -= jumlah; // Metode untuk melakukan penarikan uang dari akun (mengurangkan jumlah dari saldo).
+        }
+        else
+        {
+            Console.WriteLine("Saldo Tidak Cukup!"); // Pesan yang akan ditampilkan jika saldo tidak mencukupi untuk melakukan penarikan.
+        }
+    }
+
+    public void TampilSaldo()
+    {
+        Console.WriteLine("No. Rekening: " + noRek); // Metode untuk menampilkan nomor rekening.
+        Console.WriteLine("Saldo: Rp" + saldo); // Metode untuk menampilkan saldo akun.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("===== Selamat Datang di ATM BANK AMIKOM UNGU ====="); // Sambutan di awal program
+
+        // Membuat objek AkunBank
+        AkunBank akun = new AkunBank();
+        akun.NoRek = "6975851919";
+        akun.Pin = "340410";
+        akun.Saldo = 50000;
+
+        // Proses login
+        bool isLoggedIn = Login(akun);
+
+        if (isLoggedIn)
+        {
+            // Jika berhasil login, tampilkan menu
+            Console.WriteLine("Login berhasil!");
+
+            // Membersihkan output sebelum menampilkan menu transaksi
+            Console.Clear();
+
+            Menu(akun);
+        }
+        else
+        {
+            Console.WriteLine("Login gagal!"); // Tampil teks login gagal jika salah no.Rek & PIN
+        }
+    }
+
+
+    public static bool Login(AkunBank akun) //Metode untuk meminta user untuk input No.Rek dan PIN
+    {
+        Console.WriteLine("Silakan masukkan nomor rekening: ");
+        string inputRek = Console.ReadLine();
+        Console.WriteLine("Masukkan PIN: ");
+        string inputPin = Console.ReadLine();
+
+        if (inputRek == akun.NoRek && inputPin == akun.Pin)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Metode untuk menampilkan menu transaksi dan mengelola pilihan pengguna.
+    // Parameter akun adalah objek AkunBank yang digunakan untuk melakukan transaksi.
+    public static void Menu(AkunBank akun) 
+    {
+        // Variabel isExit digunakan untuk mengontrol perulangan while.
+        // Jika isExit bernilai false, perulangan akan terus berjalan, dan program akan tetap menampilkan menu.
+        bool isExit = false; 
+
+        while (!isExit)
+        {
+            Console.WriteLine("===== Menu Transaksi =====");
+            Console.WriteLine("1. Tarik");
+            Console.WriteLine("2. Setor");
+            Console.WriteLine("3. Tampil Saldo");
+            Console.WriteLine("4. Keluar");
+
+            Console.WriteLine("Pilih menu [1..4]: ");
+            string menuInput = Console.ReadLine();
+
+            switch (menuInput) // Control structure untuk pilihan menu
+            {
+                case "1":
+                    Console.WriteLine("Masukkan jumlah penarikan: ");
+                    decimal jumlahTarik = Convert.ToDecimal(Console.ReadLine());
+                    akun.Tarik(jumlahTarik);
+
+                    Console.WriteLine("Transaksi berhasil!");
+                    break;
+                case "2":
+                    Console.WriteLine("Masukkan jumlah setoran: ");
+                    decimal jumlahSetor = Convert.ToDecimal(Console.ReadLine());
+                    akun.Setor(jumlahSetor);
+
+                    Console.WriteLine("Transaksi berhasil!");
+                    break;
+                case "3":
+                    akun.TampilSaldo();
+                    break;
+                case "4":
+                    isExit = true;
+                    break;
+                default:
+                    Console.WriteLine("Menu tidak valid!");
+                    break;
+            }
+
+            // Tunggu pengguna menekan tombol Enter atau tombol lain sebelum kembali ke menu utama
+            Console.WriteLine("\nTekan Enter untuk kembali ke Menu Utama...");
+            Console.ReadLine();
+
+            // Bersihkan output sebelum menampilkan menu transaksi lagi
+            Console.Clear();
+        }
+
+        Console.WriteLine("Terima kasih telah menggunakan layanan kami!");
+    }
+
+
+}
